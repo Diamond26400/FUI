a286de04e1cdd00b4974d35cc05a9f1142924f0f
Diamond26400 <obaloluwajames5@gmail.com>
2023-11-13 17:18:04 +0100

    Update Target.cs

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private Rigidbody targetRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetRb = GetComponent<Rigidbody>();
+
+        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+
+        transform.position =  new Vector3(Random.Range(-4, 4), -6);
     }
 
     // Update is called once per frame
     void Update()
     {
+  
         
     }
 }
