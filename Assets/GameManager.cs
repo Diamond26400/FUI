db5114c6c4bb6e7e101a60b236b19eb43df7cfa7
Diamond26400 <obaloluwajames5@gmail.com>
2023-11-14 16:45:05 +0100

    Update GameManager.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> Targets;
+    public float spawnTime = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine((spawnRate()));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    IEnumerator spawnRate ()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnTime);
+                int index = Random.Range(0, Targets.Count);
+            Instantiate(Targets[index]);
+        }
     }
 }
