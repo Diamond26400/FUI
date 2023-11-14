3c66edc8dec10ffe44e2b084c7ae191ff6638f5d
Diamond26400 <obaloluwajames5@gmail.com>
2023-11-14 23:18:01 +0100

    Update

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> Targets;
     public float spawnRate = 1.0f;
+    public TextMeshProUGUI ScoreText;
+    private int score;
+ 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine((spawnTarget()));
+        score = 0;
+        ScoreText.text = "Score: " +score;
     }
 
     // Update is called once per frame
