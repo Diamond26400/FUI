54157e9e012848545bc34fa952e1298da1559a22
Diamond26400 <obaloluwajames5@gmail.com>
2023-11-19 17:13:49 +0100

    Update

---
+++ Assets/Difficult.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Difficult : MonoBehaviour
+{
+    private Button botton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        botton = GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
