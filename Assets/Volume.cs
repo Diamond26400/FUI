c5fba4a19fc78a44fcfe4ba28a541d57c242540c
Diamond26400 <obaloluwajames5@gmail.com>
2023-12-03 03:28:10 +0100

    update

--- Assets/Volume.cs
+++ Assets/Volume.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 public class Volume : MonoBehaviour
 {
     [SerializeField] Slider volumeSlider;
     [SerializeField] AudioSource audioSource;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +22,13 @@ public class Volume : MonoBehaviour
     void ChangeVolume(float volume)
     {
         audioSource.volume = volume;
+        Debug.Log("Volume changed: " + volume);
+        if (volume == 0)
+        {
+            volume = 0; 
+            volumeSlider.value = 0;
+          
+           
+        }
     }
 }
