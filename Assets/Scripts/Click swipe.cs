1a50786408a096c338035892fa590124310ade81
Diamond26400 <obaloluwajames5@gmail.com>
2023-12-09 16:18:00 +0100

    Update

--- Assets/Scripts/Click swipe.cs
+++ Assets/Scripts/Click swipe.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 public class Clickswipe : MonoBehaviour
@@ -9,27 +10,31 @@ public class Clickswipe : MonoBehaviour
     public float swipeThreshold = 10f; // Adjust this value to set the swipe sensitivity
     public LineRenderer lineRenderer;
     public Color swipeColor = Color.green;
-
+    public GameObject Pause;
     void Start()
     {
         lineRenderer.startColor = swipeColor;
         lineRenderer.endColor = swipeColor;
     }
 
-    void Update()
+     public void Update()
     {
         CheckForSwipe();
     }
 
-    void CheckForSwipe()
+   public void CheckForSwipe()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Pause != null && !Pause.activeSelf)
         {
-            touchStartPos = Input.mousePosition;
-            isSwiping = true;
-            lineRenderer.positionCount = 1;
-            lineRenderer.SetPosition(0, Camera.main.ScreenToWorldPoint(new Vector3(touchStartPos.x, touchStartPos.y, 10f)));
+            if (Input.GetMouseButtonDown(0))
+            {
+                touchStartPos = Input.mousePosition;
+                isSwiping = true;
+                lineRenderer.positionCount = 1;
+                lineRenderer.SetPosition(0, Camera.main.ScreenToWorldPoint(new Vector3(touchStartPos.x, touchStartPos.y, 10f)));
+            }
         }
+      
 
         if (isSwiping)
         {
